ff040473857f8d63ed4f2bf22fe64a0a8fb43c13
Mark Dickinson <marksamdickinson@gmail.com>
2020-09-21 22:29:45 -0700

    Update packages, script, use IHostApplicationLifetime over IApplicationLifetime, add configurator, and Update exension method (#5)

---
+++ DickinsonBros.Encryption.AES.Tests/Configurators/AESEncryptionServiceOptionsConfiguratorTests.cs
@@ -0,0 +1,65 @@
+﻿using DickinsonBros.Encryption.AES.Configurators;
+using DickinsonBros.Encryption.AES.Models;
+using DickinsonBros.Test;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DickinsonBros.Encryption.AES.Tests.Configurators
+{
+    [TestClass]
+    public class AESEncryptionServiceOptionsConfiguratorTests : BaseTest
+    {
+        public class TestClass
+        {
+
+        }
+
+        [TestMethod]
+        public async Task Configure_Runs_OptionsReturned()
+        {
+            var certificateEncryptionServiceOptionsOfObject = new AESEncryptionServiceOptions<TestClass>
+            {
+                InitializationVector = "SampleInitializationVector",
+                Key = "SampleKey"
+            };
+            var configurationRoot = BuildConfigurationRoot(certificateEncryptionServiceOptionsOfObject);
+
+            await RunDependencyInjectedTestAsync
+            (
+                async (serviceProvider) =>
+                {
+                    //Setup
+
+                    //Act
+                    var options = serviceProvider.GetRequiredService<IOptions<AESEncryptionServiceOptions<TestClass>>>().Value;
+
+                    //Assert
+                    Assert.IsNotNull(options);
+
+                    Assert.AreEqual(certificateEncryptionServiceOptionsOfObject.InitializationVector, options.InitializationVector);
+                    Assert.AreEqual(certificateEncryptionServiceOptionsOfObject.Key, options.Key);
+
+                    await Task.CompletedTask.ConfigureAwait(false);
+
+                },
+                serviceCollection => ConfigureServices(serviceCollection, configurationRoot)
+            );
+        }
+
+        #region Helpers
+
+        private IServiceCollection ConfigureServices(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddOptions();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddSingleton<IConfigureOptions<AESEncryptionServiceOptions<TestClass>>, AESEncryptionServiceOptionsConfigurator<TestClass>>();
+
+            return serviceCollection;
+        }
+
+        #endregion
+    }
+}
--- DickinsonBros.Encryption.AES.Tests/Extensions/IServiceCollectionExtensionsTests.cs
+++ DickinsonBros.Encryption.AES.Tests/Extensions/IServiceCollectionExtensionsTests.cs
@@ -1,6 +1,9 @@
 ﻿using DickinsonBros.Encryption.AES.Abstractions;
+using DickinsonBros.Encryption.AES.Configurators;
 using DickinsonBros.Encryption.AES.Extensions;
+using DickinsonBros.Encryption.AES.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
@@ -9,6 +12,8 @@ namespace DickinsonBros.Encryption.AES.Tests.Extensions
     [TestClass]
     public class IServiceCollectionExtensionsTests
     {
+        public class Sample { };
+
         [TestMethod]
         public void AddAESEncryptionService_Should_Succeed()
         {
@@ -16,13 +21,17 @@ namespace DickinsonBros.Encryption.AES.Tests.Extensions
             var serviceCollection = new ServiceCollection();
 
             // Act
-            serviceCollection.AddAESEncryptionService<object>();
+            serviceCollection.AddAESEncryptionService<Sample>();
 
             // Assert
 
-            Assert.IsTrue(serviceCollection.Any(serviceDefinition => serviceDefinition.ServiceType == typeof(IAESEncryptionService<>) &&
-                                           serviceDefinition.ImplementationType == typeof(AESEncryptionService<>) &&
+            Assert.IsTrue(serviceCollection.Any(serviceDefinition => serviceDefinition.ServiceType == typeof(IAESEncryptionService<Sample>) &&
+                                           serviceDefinition.ImplementationType == typeof(AESEncryptionService<Sample>) &&
                                            serviceDefinition.Lifetime == ServiceLifetime.Singleton));
+
+            Assert.IsTrue(serviceCollection.Any(serviceDefinition => serviceDefinition.ServiceType == typeof(IConfigureOptions<AESEncryptionServiceOptions<Sample>>) &&
+                   serviceDefinition.ImplementationType == typeof(AESEncryptionServiceOptionsConfigurator<Sample>) &&
+                   serviceDefinition.Lifetime == ServiceLifetime.Singleton));
         }
     }
 }
